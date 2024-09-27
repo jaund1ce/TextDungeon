241131e2eef8ed5b40dc32bb26166d37a7f48478
yellow-redcard <iclever0507@gmail.com>
2024-09-27 15:18:08 +0900

    (Fix)PlayerClass.cs

---
+++ TextDungeon/TextDungeon/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Player
+{
+    public string Name { get; set; }
+    public string Class { get; set; }
+    public int Level { get; set; } = 1; 
+    public int Attack { get; set; } = 10;
+    public int Defense { get; set; } = 5;
+    public int Health { get; set; } = 100;
+    public int Gold { get; set; } = 1500;
+}
