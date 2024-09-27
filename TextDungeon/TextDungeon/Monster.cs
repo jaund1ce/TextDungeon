1bd2b2f6e17cda9339a37b8f773e22e3c6f042eb
KimSungHo-web <epdlam20@gmail.com>
2024-09-27 15:07:58 +0900

    (New)Monster.cs

---
+++ TextDungeon/TextDungeon/Monster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Monster
+{
+    public string Name { get; }
+    public int Health { get; private set; }
+    public int AttackPower { get; }
+    public int Level { get; }
+
+    public Monster(string name, int health, int attackPower, int level)
+    {
+        Name = name;
+        Health = health;
+        AttackPower = attackPower;
+        Level = level;
+    }
+
+    public bool IsDead()
+    {
+        return Health <= 0;
+    }
+
+    public int Attack()
+    {
+        Random rand = new Random();
+        return rand.Next(AttackPower - 1, AttackPower + 2); // 공격력 10% 오차
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health < 0) Health = 0;
+    }
+}
