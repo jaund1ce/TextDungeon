d150fbf690a2b20e9a124e41e9b50569af18fbff
Ban-GunOo <slqdydskq@naver.com>
2024-10-01 16:50:22 +0900

    inven store

---
+++ TextDungeon/TextDungeon/Item.cs
@@ -0,0 +1,30 @@
+﻿namespace TextDungeon;
+
+public class Item
+{
+    public string itemName { get; }
+    public int itemType { get; }
+    public int itemValue { get; }
+    public string itemDesc { get; }
+    public int itemPrice { get; }
+
+    public Item(string itemname, int itemtype, int itemvalue, string itemdesc, int itemprice)
+    {
+        itemName = itemname;
+        itemType = itemtype;
+        itemValue = itemvalue;
+        itemDesc = itemdesc;
+        itemPrice = itemprice;
+    }
+    public string DisplayTypeText
+    {
+        get
+        {
+            return itemType == 0 ? "공격력" : "방어력";
+        }
+    }
+    public string ItemInfoText()
+    {
+        return $"{itemName}  |  {DisplayTypeText} +{itemValue}  |  {itemDesc}";
+    }
+}
